62f28f98e723042a7ffe59e3053955d35642221c
nharenberg <n.harenberg@live.com>
2016-08-16 16:23:24 -0700

    added CRUD for credit

--- routes/credits.js
+++ routes/credits.js
@@ -13,13 +13,29 @@ router.post("/", (req, res) => {
   })
 })
 
-
-/////////////////
 router.get("/", (req, res) => {
   Trans.find({}, (err, trans) => {
     return res.status(err ? 500 :200).send(err || trans)
-
   })
 });
+
+router.put("/:id", (req, res) => {
+  // console.log("req.body:", req.body)
+  //   console.log("req.params:", req.params.id)
+  Trans.findByIdAndUpdate(req.params.id ,req.body, (err, trans) => {
+    return res.status(err ? 500 :200).send(err || trans)
+  })
+});
+
+router.delete("/:id", (req, res) => {
+  Trans.findByIdAndRemove(
+    req.params.id,
+    (err) => {
+      if (err) return res.status(400).send(err);
+      return res.status(200).send("Successful Delete");
+    }
+  )
+})
+
 
 module.exports = router;
